9553b952cc997d52e240434ee11526c269d62575
Mannopson <mannopson@yahoo.com>
2021-07-17 09:57:41 +0500

    Added an activity indicator

---
+++ WeatherAPI/ActivityView.swift
@@ -0,0 +1,8 @@
+//
+//  ActivityView.swift
+//  WeatherAPI
+//
+//  Created by AzizOfficial on 7/17/21.
+//
+
+import Foundation
--- WeatherAPI/ViewController.swift
+++ WeatherAPI/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    lazy var activityIndicator: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView.init(style: .large)
+        activityIndicator.color = UIColor.white
+        activityIndicator.startAnimating()
+        return activityIndicator
+    }()
+    
+    lazy var activityView: UIView = {
+        let activityView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height))
+        activityView.backgroundColor = UIColor.systemTeal
+        return activityView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.addSubview(activityView)
+        activityIndicator.center = activityView.center
+        activityView.addSubview(activityIndicator)
     }
-
-
 }
 
